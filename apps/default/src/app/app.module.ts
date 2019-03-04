6325920c34caa8fd1ba90619c496db525776007a
Anupam Agnihotri <anupamagnihotri@iMac.local>
2019-03-04 13:38:44 +0530

    added material and html css

--- apps/default/src/app/app.module.ts
+++ apps/default/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import {SpanishLandingViewModule} from '@view-sandbox/spanish-landing-view'
+import {SpanishLandingViewModule} from '@view-sandbox/spanish-landing-view';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule ,SpanishLandingViewModule ],
+  imports: [BrowserModule ,SpanishLandingViewModule, BrowserAnimationsModule ],
   providers: [],
   bootstrap: [AppComponent]
 })

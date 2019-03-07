9beb17eb3c8ce07c9148950873cced0ab3881bcb
Anupam Agnihotri <xanukum@gmail.com>
2019-03-07 14:20:41 +0530

    updated code

--- libs/spanish-landing-view/src/lib/spanish-landing-view.module.ts
+++ libs/spanish-landing-view/src/lib/spanish-landing-view.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
-  imports: [CommonModule, MatIconModule],
-  declarations: [MainComponent],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatIconModule,
+    MatFormFieldModule,
+MatInputModule,MatTabsModule,LayoutModule,MatCardModule,MatChipsModule,
+
+ 
+  ],
+  declarations: [MainComponent, HeaderComponent, FooterComponent],
   exports:[MainComponent]
 })
 export class SpanishLandingViewModule {}

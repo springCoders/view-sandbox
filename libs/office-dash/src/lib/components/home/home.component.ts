6c17951531c3ff1387c7cc9f1da7d9d4e1e04f6d
hrstackasian@anukum <hr.stackasian@gmail.com>
2019-03-16 18:45:14 +0530

    css update and animation

--- libs/office-dash/src/lib/components/home/home.component.ts
+++ libs/office-dash/src/lib/components/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,HostListener, ElementRef } from '@angular/core';
+import { AnimationEvent } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+ 
 @Component({
   selector: 'officeintegrator-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        opacity: 1,
+      })),
+      state('closed', style({
+         transform:' rotateZ( 180deg )',            
+        transition: 'transform 150ms ease-in'
+      })),
+      transition('open => closed', [
+        animate('.05s')
+      ]),
+      transition('closed => open', [
+        animate('0.05s')
+      ]),
+    ]),
+  ], 
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeComponent {
+  isOpen = true;
+  fireEvent(e){
+    this.isOpen = !this.isOpen;
   }
-
+   
 }

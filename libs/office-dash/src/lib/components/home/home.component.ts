0ba1dfca8ad0798d4466f3fa496b215c049649d9
xcodeanupam <xcodeanupam@gmail.com>
2019-04-08 14:15:50 +0530

    sd

--- libs/office-dash/src/lib/components/home/home.component.ts
+++ libs/office-dash/src/lib/components/home/home.component.ts
@@ -22,7 +22,7 @@ declare var TweenMax: any;
   //       opacity: 1,
   //     })),
   //     state('closed', style({
-  //        transform:' rotateY( 180deg )',            
+  //        transform:' rotateY( 180deg )',
   //       transition: 'transform 150ms ease-in'
   //     })),
   //     transition('open => closed', [
@@ -32,7 +32,7 @@ declare var TweenMax: any;
   //       animate('0.05s')
   //     ]),
   //   ]),
-  // ], 
+  // ],
 })
 
 export class HomeComponent implements OnInit  {
@@ -55,20 +55,20 @@ export class HomeComponent implements OnInit  {
   // @ViewChild('slider') sliderEl: ElementRef;
   eventText = '';
   indicators;
-   
+
   zoom: Boolean = false;
   vv: any;
-   
+
 
   items = [
     { title: 'Slide 1', image: 'assets/laptop1.jpeg' },
     { title: 'Slide 2', image: 'assets/laptop2.jpeg' },
     { title: 'Slide 3', image: 'assets/laptop1.jpeg' },
   ]
-  
+
   addSlide() {
     this.items.push({
-      title: `Slide 4` , image: 'assets/laptop2.jpeg' 
+      title: `Slide 4` , image: 'assets/laptop2.jpeg'
     });
   }
 
@@ -90,14 +90,16 @@ whatIdo = [
   { service: 'GRAPHIC DESIGN', image: 'add_to_queue', description: 'Proin a ullamcorper et primis lobortis laoreet senectus. Vitae dignissim sollicitudin eleifend cursus tempus curabitur posuere nam arcu platea sodales.'},
   { service: 'BRANDING', image: 'add_to_queue', description: 'Proin a ullamcorper et primis lobortis laoreet senectus. Vitae dignissim sollicitudin eleifend cursus tempus curabitur posuere nam arcu platea sodales.'},
   { service: 'WEB DESIGN', image: 'add_to_queue', description: 'Proin a ullamcorper et primis lobortis laoreet senectus. Vitae dignissim sollicitudin eleifend cursus tempus curabitur posuere nam arcu platea sodales.'},
-  { service: 'OPENCART', image: 'add_to_queue', description: 'Proin a ullamcorper et primis lobortis laoreet senectus. Vitae dignissim sollicitudin eleifend cursus tempus curabitur posuere nam arcu platea sodales.'}, 
+  { service: 'OPENCART', image: 'add_to_queue', description: 'Proin a ullamcorper et primis lobortis laoreet senectus. Vitae dignissim sollicitudin eleifend cursus tempus curabitur posuere nam arcu platea sodales.'},
 ];
 
 client =[
   {image:"assets/01.jpg", name:'Grace Ruby', comment:'Sem duis platea erat feugiat vivamus nascetur sapien tortor. Sollic dictum ultric. Aliquam inceptos bibendum fringilla sodales. Molest lacin urna per aenean commodo sociosqu.'},
+  {image:"assets/01.jpg", name:'Grace Ruby', comment:'Sem duis platea erat feugiat vivamus nascetur sapien tortor. Sollic dictum ultric. Aliquam inceptos bibendum fringilla sodales. Molest lacin urna per aenean commodo sociosqu.'},
+  {image:"assets/01.jpg", name:'Grace Ruby', comment:'Sem duis platea erat feugiat vivamus nascetur sapien tortor. Sollic dictum ultric. Aliquam inceptos bibendum fringilla sodales. Molest lacin urna per aenean commodo sociosqu.'},
   {image:"assets/01.jpg", name:'Grace Ruby', comment:'Sem duis platea erat feugiat vivamus nascetur sapien tortor. Sollic dictum ultric. Aliquam inceptos bibendum fringilla sodales. Molest lacin urna per aenean commodo sociosqu.'}
 ]
- 
+
 
 
 
@@ -107,87 +109,86 @@ client =[
   }
   mouseenter(){ }
   mouseleave(){}
-  
-  
 
-constructor() { 
+
+
+constructor() {
     this.indicators = new Indicator();
-
-    // Hammer.on(window, "load resize scroll", function(ev) {
-    
-    //   var myElement = document.getElementById('inTry');
-    //   var myElement2 = document.getElementById('inTry2');
-    //   //     var hammertime = new Hammer(myElement);
-    //   //     hammertime.on('pan', function(ev) {
-    //   // 	console.log(ev);
-    //   // });
-      
-    //   var mc = new Hammer.Manager(myElement, {
-    //     recognizers: [ 
-    //       [Hammer.Rotate],
-    //       [Hammer.Pinch, { enable: false }, ['rotate']],
-    //       [Hammer.Swipe,{ direction: Hammer.DIRECTION_HORIZONTAL }],
-    //     ]
-    //   });
-    //   var hammertime = new Hammer(document.getElementById('inTry'));
-    //   hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
-    //   hammertime.get('pinch').set({ enable: true });
-    //   hammertime.get('rotate').set({ enable: true });
-    //   hammertime.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });       // var pinch = new Hammer.Pinch();
-    //   // mc.add(pinch);
-    //   mc.add( new Hammer.Swipe({ direction: Hammer.DIRECTION_ALL, threshold: 0, pointers: 0 }) );
-    //   mc.add( new Hammer.Tap({ event: 'quadrupletap', taps: 4 }) );
-    //   var lastCenter;
-
-    //   function onDrag(ev){
-
-    //     var center = ev.gesture.center;
-    //     if (lastCenter) 
-    //     myElement2.innerHTML = "hello inner";
-    //     lastCenter = center;
-    // }
-
-    //   mc.on("Swipe", onDrag);
-      
-    //   console.log(ev.type);
-    // });
-    Hammer.on(window, "load resize scroll", function(ev) {
-    // var sliderEl = document.querySelector( '.slider' );
-    var sliderEl = document.getElementById( 'slider' );
-    var slideCount = 3;
-    var activeSlide = 0; // NEW: the current slide # (0 = first)
-    var sliderManager = new Hammer.Manager( sliderEl );
-    sliderManager.add( new Hammer.Pan({ threshold: 0, pointers: 0 }) );
-    sliderManager.on( 'pan', function( e ) {
-      var percentage = 100 / slideCount * e.deltaX / window.innerWidth;
-      var transformPercentage = percentage - 100 / slideCount * activeSlide; // NEW
-      sliderEl.style.transform = 'translateX( ' + transformPercentage + '% )';
-      if( e.isFinal ) { // NEW: this only runs on event end
-        if( percentage < 0 )
-          goToSlide( activeSlide + 1 );
-        else if( percentage > 0 )
-          goToSlide( activeSlide - 1 );
-        else
-          goToSlide( activeSlide );
-      }
-    });
-    
-    // NEW: function that changes the slide
-    var goToSlide = function( number ) {
-      if( number < 0 )
-        activeSlide = 0;
-      else if( number > slideCount - 1 )
-        activeSlide = slideCount - 1
-      else
-        activeSlide = number;
-    
-     var percentage = -( 100 / slideCount ) * activeSlide;
-     sliderEl.style.transform = 'translateX( ' + percentage + '% )';
-    };
-    });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    Hammer.on(window, "load resize scroll", function(ev) {
+      // var sliderEl = document.querySelector( '.slider' );
+      let sliderEl = document.getElementById( 'slider' );
+      let slideCount = 3;
+      let activeSlide = 0; // NEW: the current slide # (0 = first)
+      let sliderManager = new Hammer.Manager( sliderEl );
+      sliderManager.add( new Hammer.Pan({ threshold: 0, pointers: 0 }) );
+      sliderManager.on( 'pan', function( e ) {
+        let percentage = 100 / slideCount * e.deltaX / window.innerWidth;
+        let transformPercentage = percentage - 100 / slideCount * activeSlide; // NEW
+        sliderEl.style.transform = 'translateX( ' + transformPercentage + '% )';
+        if( e.isFinal ) { // NEW: this only runs on event end
+          if( percentage < 0 )
+            goToSlide( activeSlide + 1 );
+          else if( percentage > 0 )
+            goToSlide( activeSlide - 1 );
+          else
+            goToSlide( activeSlide );
+        }
+      });
+      // NEW: function that changes the slide
+      let goToSlide = function( number ) {
+        if( number < 0 )
+          activeSlide = 0;
+        else if( number > slideCount - 1 )
+          activeSlide = slideCount - 1
+        else
+          activeSlide = number;
+          let percentage = -( 100 / slideCount ) * activeSlide;
+       sliderEl.style.transform = 'translateX( ' + percentage + '% )';
+      };
+      });
+
+      Hammer.on(window, "load resize scroll", function(evt) {
+        // var sliderEl = document.querySelector( '.slider' );
+        let sliderElt = document.getElementById( 'sliderTwo' );
+        let slideCount = 2;
+        let activeSlide = 0; // NEW: the current slide # (0 = first)
+        let sliderManager = new Hammer.Manager( sliderElt );
+        sliderManager.add( new Hammer.Pan({ threshold: 0, pointers: 0 }) );
+        sliderManager.on( 'pan', function( e ) {
+          let percentage = 100 / slideCount * e.deltaX / window.innerWidth;
+          let transformPercentage = percentage - 100 / slideCount * activeSlide; // NEW
+          sliderElt.style.transform = 'translateX( ' + transformPercentage + '% )';
+          if( e.isFinal ) { // NEW: this only runs on event end
+            if( percentage < 0 )
+              goToSlideTwo( activeSlide + 1 );
+            else if( percentage > 0 )
+              goToSlideTwo( activeSlide - 1 );
+            else
+              goToSlideTwo( activeSlide );
+          }
+        });
+
+        // NEW: function that changes the slide
+        let goToSlideTwo = function( number ) {
+          if( number < 0 )
+            activeSlide = 0;
+          else if( number > slideCount - 1 )
+            activeSlide = slideCount - 1
+          else
+            activeSlide = number;
+
+            let percentage = -( 100 / slideCount ) * activeSlide;
+            sliderElt.style.transform = 'translateX( ' + percentage + '% )';
+        };
+        });
+  }
+  ngAfterViewInit(): void {
+
+
+  }
 
   imageZoom(vk){
     this.zoom = true;
@@ -196,8 +197,8 @@ constructor() {
   close(){
     this.zoom = false;
   }
- 
- 
+
+
 push() {
   this.avatars.push(
     {
